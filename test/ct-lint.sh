8612338a043ccae5f3bdb2a695da077125ac6a62
Wylie Hobbs <wylie-hobbs@pluralsight.com>
2020-03-04 13:30:01 -0700

    Add better debug output

--- test/ct-lint.sh
+++ test/ct-lint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 MODIFIED=$(test/bin/git_diff.sh)
-echo "${MODIFIED}"
+echo "****************************"
+echo "Modified charts: ${MODIFIED}"
+echo "****************************"
 
 for c in "${MODIFIED[@]}"; do
-  ct lint --config test/ct.yaml --charts charts/"${c}"
+  ct lint --config test/ct.yaml --charts "charts/${c}"
 done
